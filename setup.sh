8253660bd306e46ed8cb7e78d196260e6ba82f80
Alexander Berg <60197499+calexanderberg@users.noreply.github.com>
2022-06-23 20:35:24 +0200

    Added more formulae and some comment

--- setup.sh
+++ setup.sh
@@ -1,21 +1,22 @@
 
-#A script to help me setup future Macs
-#Install the script on a usb and run it.
+# A script to help me setup future Macs
+# Install the script on a usb and run it.
 
 echo -e "Hello and welcome to macoser\n"
 echo -e "In the begining we will need your sudo password, so please be ready to help us here in the begining.\n"
 
-#Needed to run the script
+# Needed to run the script
 xcode-select --install
 
-#Install Oh My Zsh
+# Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#Install brew
+# Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo -e "All sudo required stuff should be over\n"
 
+# Creating aliases I use
 echo -e "Creating aliases\n"
 
 echo "#aliases" >>~/.zshrc
@@ -26,7 +27,6 @@ echo "alias Downloads=\"cd ~/Downloads\"" >>~/.zshrc
 echo "alias brews=\"brew update; brew upgrade; brew cleanup; brew autoremove; brew doctor\"" >>~/.zshrc
 
 # I believe none of these should require sudo
-
 echo -e "Installing brew casks\n"
 CASKS=(
     adobe-creative-cloud
@@ -48,20 +48,25 @@ CASKS=(
 
 brew cask install ${CASKS[@]}
 
-
+# Installing brew formulae
 echo -e "Installing brew formulae\n"
 FORMULAE=(
-  chruby
+  chruby            
   elixir
-  htop
-  neofetch
-  node
-  rbenv
-  speedtest-cli
-  wifi-password
+  go
+  htop              
+  neofetch          
+  node              
+  rbenv             
+  rust               
+  speedtest-cli    
+  verilator        
+  wifi-password     
 )
 
 brew install ${FORMULAE[@]}
+
+# Installing a better ruby, I have had problems with gem on every Mac I've used
 
 echo -e "Changing ruby, to make gem work.\n"
 rbenv install 3.1.2
